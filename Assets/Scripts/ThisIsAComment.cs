ff62ba949ac54d84859998456150ddb758edda2d
NathanBunTV <nanobuntv.contact@gmail.com>
2023-01-19 07:24:19 -0600

    Assets for v1.0.3

---
+++ Assets/Scripts/ThisIsAComment.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+// Attach this script to any GameObject for which you want to put a note.
+public class ThisIsAComment : MonoBehaviour
+{
+    [TextArea]
+    public string Comment = "";
+}
