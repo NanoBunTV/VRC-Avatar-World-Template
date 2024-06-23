19093d500267a9da180c5b4be191e070dd29518c
NanoBunTV <contact@nanobun.tv>
2024-06-23 06:05:13 -0500

    Reupload all files

--- Assets/Scripts/ThisIsAComment.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-// Attach this script to any GameObject for which you want to put a note.
-public class ThisIsAComment : MonoBehaviour
-{
-    [TextArea]
-    public string Comment = "";
-}
